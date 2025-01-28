b1df7be37a958b5c6315e913d7362488b26ce12a
ravicharanG <ravicharan0518@gmail.com>
2025-01-28 16:09:10 +0530

    let,const and var keywords

---
+++ index.js
@@ -0,0 +1,21 @@
+const accountId =144563
+let accountEmail ="xy@z.com"
+var accountPassword = "12345"
+accountCity ="jaipur"
+let accountState;
+
+
+// accountId = 2 // not allowed
+
+accountEmail="h2@gmail.com"
+accountPassword="213444"
+accountCity ="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
